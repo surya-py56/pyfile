b0f5d200883847da261c316367ad954cee4c45b0
surya-py56 <surya.exe0156@gmail.com>
2023-06-08 11:09:29 +0530

    Create scc.py

---
+++ scc.py
@@ -0,0 +1,19 @@
+#Webscrapping
+def wikisearch():
+    import requests
+    inp=input("Enter topic to search (divide words with '_'): ")
+    url=f"https://en.wikipedia.org/wiki/{inp}"
+    res=requests.get(url)
+    from bs4 import BeautifulSoup
+    import html5lib
+    soup=BeautifulSoup(res.content,'html5lib')
+    l=soup.find_all('p')
+    nl=[]
+    for i in l:
+        nl.append(i.get_text())
+    print(nl[1])
+    print(nl[2])
+    
+wikisearch()
+    
+    
